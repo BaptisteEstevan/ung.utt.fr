158cface7151aee9c1c6dcd1566f13e76b10e01d
nashe <contact@nashe.fr>
2015-10-30 14:41:15 +0100

    Update $DEPLOY_PATH

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tar -czf package.tgz _site
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 2222 package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 2222 package.tgz extract-package.sh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 2222 $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/extract-package.sh
